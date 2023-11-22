16a30848a49d443ca42253502aa216e042cd9e28
Byte Bender <136265142+BYT-Bender@users.noreply.github.com>
2023-11-23 01:16:55 +0530

    Update series.js

--- series.js
+++ series.js
@@ -1,23 +1,26 @@
-const INDEX = document.querySelector(".index");
-const NUMBER = document.querySelector(".number");
+const INDEX = document.querySelector("#index");
+const NUMBER = document.querySelector("#number");
 
 function FIB() {
-    const number = 100;
-    let n1 = 0, n2 = 1, nextTerm;
+    const number = 50;
+    let n0 = 0, n1 = 0, n2 = 1, nextTerm;
     let i = 1;
 
     function addFibonacciNumber() {
         if (i <= number) {
 
-            INDEX.innerHTML = i;
-            NUMBER.innerHTML = n1
+            // INDEX.innerHTML = i;
+            // NUMBER.innerHTML = n1
+            animateValue(INDEX, i-1, i, 500);
+            animateValue(NUMBER, n0, n1, 500);
 
             nextTerm = n1 + n2;
+            n0 = n1
             n1 = n2;
             n2 = nextTerm;
             i++;
 
-            setTimeout(addFibonacciNumber, 200);
+            setTimeout(addFibonacciNumber, 1000);
         }
     }
 
